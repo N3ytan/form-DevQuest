5b82164e085e9dd5909c24a7b2032962a59daf91
n3ytan <109997116+N3ytan@users.noreply.github.com>
2023-01-12 08:01:50 -0300

    js

---
+++ src/js/form.js
@@ -0,0 +1,15 @@
+const form = document.querySelector("#form")
+const inputNome = document.querySelector("#nome")
+const inputEmail = document.querySelector("#email")
+const inputTelefone = document.querySelector("#telefone")
+const inputMensagem = document.querySelector("#msg")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkinputs()
+})
+
+function checkinputs() {
+
+}
