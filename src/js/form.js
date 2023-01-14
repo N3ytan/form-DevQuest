30a58c0cab6b84fa9338338dde16aa4a3300e121
n3ytan <109997116+N3ytan@users.noreply.github.com>
2023-01-14 08:20:55 -0300

    starting and input validations

--- src/js/form.js
+++ src/js/form.js
@@ -1,15 +1,40 @@
-const form = document.querySelector("#form")
-const inputNome = document.querySelector("#nome")
-const inputEmail = document.querySelector("#email")
-const inputTelefone = document.querySelector("#telefone")
-const inputMensagem = document.querySelector("#msg")
+const form = document.querySelector("#form");
+const inputNome = document.querySelector("#nome");
+const inputEmail = document.querySelector("#email");
+const inputTelefone = document.querySelector("#telefone");
+const inputMensagem = document.querySelector("#msg");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    checkinputs()
-})
+  verificarInputs();
+});
 
-function checkinputs() {
+function verificarInputs() {
+  const nomeValue = nome.value;
+  const emailValue = email.value;
+  const telefoneValue = telefone.value;
+  const mensagemValue = msg.value;
 
+  if (nomeValue === "") {
+    selecionarMensagemDeErro();
+  } else {
+    selecionarCampoAprovado();
+  }
 }
+
+function selecionarMensagemDeErro() {
+  const campoNome = document.querySelector("#nome");
+  const campoSmall = document.querySelector("small");
+
+  campoNome.classList.add("invalido");
+  campoSmall.classList.add("negado");
+}
+
+// function selecionarCampoAprovado() {
+//   const campoNome = document.querySelector("#nome");
+//   const campoSmall = document.querySelector("small");
+
+//   campoNome.classList.add("valido");
+//   campoSmall.classList.add("aprovado");
+// }
