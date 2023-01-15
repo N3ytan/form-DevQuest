2c131674a551ddf5f47f5869006e04e39d383f72
n3ytan <109997116+N3ytan@users.noreply.github.com>
2023-01-14 23:16:14 -0300

    input settings

--- src/js/form.js
+++ src/js/form.js
@@ -11,37 +11,88 @@ let mensagemValue = msg.value;
 let limpaCor = false;
 
 const campoNome = document.querySelector("#nome");
+const campoEmail = document.querySelector("#email");
+const campoTelefone = document.querySelector("#telefone");
+const campoMensagem = document.querySelector("#msg");
 const campoSmall = document.querySelector("small");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   verificarCaracteres();
   if (limpaCor) {
-    LimpaClasse();
+    limpaClasse();
     limpaCor = false;
   }
-  VerificarNome();
+
+  verificarNome();
+  verificarEmail();
+  verificarTelefone();
+  verificarMensagem();
 });
 
 let verificarCaracteres = () => {
-  console.log(nome.value);
+  console.log(nome.value, email.value, telefone.value, msg.value);
 };
 
-const VerificarNome = () => {
+const verificarNome = () => {
   if (nome.value == "") {
     campoNome.classList.add("invalido");
     campoSmall.classList.add("negado");
     limpaCor = true;
   } else {
     campoNome.classList.add("valido");
-    campoNome.classList.add("aprovado");
     limpaCor = true;
   }
 };
 
-function LimpaClasse() {
+const verificarEmail = () => {
+  if (nome.value == "") {
+    campoEmail.classList.add("invalido");
+    campoSmall.classList.add("negado");
+    limpaCor = true;
+  } else {
+    campoEmail.classList.add("valido");
+    limpaCor = true;
+  }
+};
+
+const verificarTelefone = () => {
+  if (telefone.value == "") {
+    campoTelefone.classList.add("invalido");
+    campoSmall.classList.add("negado");
+    limpaCor = true;
+  } else {
+    campoTelefone.classList.add("valido");
+    limpaCor = true;
+  }
+};
+
+const verificarMensagem = () => {
+  if (msg.value == "") {
+    campoMensagem.classList.add("invalido");
+    campoSmall.classList.add("negado");
+    limpaCor = true;
+  } else {
+    campoMensagem.classList.add("valido");
+    limpaCor = true;
+  }
+};
+
+function limpaClasse() {
   campoNome.classList.remove("invalido");
   campoSmall.classList.remove("negado");
   campoNome.classList.remove("valido");
-  campoSmall.classList.remove("aprovado");
+
+  campoEmail.classList.remove("invalido");
+  campoSmall.classList.remove("negado");
+  campoEmail.classList.remove("valido");
+
+  campoTelefone.classList.remove("invalido");
+  campoSmall.classList.remove("negado");
+  campoTelefone.classList.remove("valido");
+
+  campoMensagem.classList.remove("invalido");
+  campoSmall.classList.remove("negado");
+  campoMensagem.classList.remove("valido");
 }
