96ba305d430b4f58b7f94fb0c2be72f54a382167
n3ytan <109997116+N3ytan@users.noreply.github.com>
2023-01-14 21:06:21 -0300

    fixing the JS

--- src/js/form.js
+++ src/js/form.js
@@ -4,37 +4,45 @@ const inputEmail = document.querySelector("#email");
 const inputTelefone = document.querySelector("#telefone");
 const inputMensagem = document.querySelector("#msg");
 
+let nomeValue = nome.value;
+let emailValue = email.value;
+let telefoneValue = telefone.value;
+let mensagemValue = msg.value;
+let limpaCor = false;
+
+const campoNome = document.querySelector("#nome");
+const campoSmall = document.querySelector("small");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  verificarInputs();
+  verificarCaracteres();
+  if (limpaCor) {
+    LimpaClasse();
+    limpaCor = false;
+  }
+  VerificarValidacao();
 });
 
-function verificarInputs() {
-  const nomeValue = nome.value;
-  const emailValue = email.value;
-  const telefoneValue = telefone.value;
-  const mensagemValue = msg.value;
+const verificarCaracteres = () => {
+  console.log(nome.value);
+};
 
-  if (nomeValue === "") {
-    selecionarMensagemDeErro();
+const VerificarValidacao = () => {
+  if (nome.value == "") {
+    campoNome.classList.add("invalido");
+    campoSmall.classList.add("negado");
+    limpaCor = true;
   } else {
-    selecionarCampoAprovado();
+    campoNome.classList.add("valido");
+    campoNome.classList.add("aprovado");
+    console.log("teste");
+    limpaCor = true;
   }
+};
+
+function LimpaClasse() {
+  campoNome.classList.remove("invalido");
+  campoSmall.classList.remove("negado");
+  campoNome.classList.remove("valido");
+  campoSmall.classList.remove("aprovado");
 }
-
-function selecionarMensagemDeErro() {
-  const campoNome = document.querySelector("#nome");
-  const campoSmall = document.querySelector("small");
-
-  campoNome.classList.add("invalido");
-  campoSmall.classList.add("negado");
-}
-
-// function selecionarCampoAprovado() {
-//   const campoNome = document.querySelector("#nome");
-//   const campoSmall = document.querySelector("small");
-
-//   campoNome.classList.add("valido");
-//   campoSmall.classList.add("aprovado");
-// }
