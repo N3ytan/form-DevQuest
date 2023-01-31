63442566084f97e7f9137240438959412093ae67
n3ytan <109997116+N3ytan@users.noreply.github.com>
2023-01-31 20:55:18 -0300

    adjusts

--- src/js/form.js
+++ src/js/form.js
@@ -1,88 +1,21 @@
-const form = document.querySelector("#form");
-const campoNome = document.querySelector("#nome");
-const campoEmail = document.querySelector("#email");
-const campoTelefone = document.querySelector("#telefone");
-const campoMensagem = document.querySelector("#msg");
-const campoSmall = document.querySelector("small");
-let limpaCor = false;
+const inputs = document.querySelectorAll(".dados");
+console.log(inputs);
+
+const campoSmall = document.querySelectorAll(".small")
+console.log(campoSmall)
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  verificarCaracteres();
-  if (limpaCor) {
-    limpaClasse();
-    limpaCor = false;
-  }
-
-  verificarNome();
-  verificarEmail();
-  verificarTelefone();
-  verificarMensagem();
+  inputs.forEach((input, index) => {
+    
+    if (input.value === "") {
+      input.classList.add("invalido");
+      campoSmall[index].classList.add("negado");
+      console.log(index);
+    } else {
+      input.classList.add("valido");
+      campoSmall[index].classList.remove("negado");
+    }
+  });
 });
 
-let verificarCaracteres = () => {
-  console.log(nome.value, email.value, telefone.value, msg.value);
-};
-
-const verificarNome = () => {
-  if (nome.value == "") {
-    campoNome.classList.add("invalido");
-    campoSmall.classList.add("negado");
-    limpaCor = true;
-  } else {
-    campoNome.classList.add("valido");
-    limpaCor = true;
-  }
-};
-
-const verificarEmail = () => {
-  if (nome.value == "") {
-    campoEmail.classList.add("invalido");
-    campoSmall.classList.add("negado");
-    limpaCor = true;
-  } else {
-    campoEmail.classList.add("valido");
-    limpaCor = true;
-  }
-};
-
-const verificarTelefone = () => {
-  if (telefone.value == "") {
-    campoTelefone.classList.add("invalido");
-    campoSmall.classList.add("negado");
-    limpaCor = true;
-  } else {
-    campoTelefone.classList.add("valido");
-    limpaCor = true;
-  }
-};
-
-const verificarMensagem = () => {
-  if (msg.value == "") {
-    campoMensagem.classList.add("invalido");
-    campoSmall.classList.add("negado");
-    limpaCor = true;
-  } else {
-    campoMensagem.classList.add("valido");
-    limpaCor = true;
-  }
-};
-
-function limpaClasse() {
-  campoNome.classList.remove("invalido");
-  campoSmall.classList.remove("negado");
-  campoNome.classList.remove("valido");
-
-  campoEmail.classList.remove("invalido");
-  campoSmall.classList.remove("negado");
-  campoEmail.classList.remove("valido");
-
-  campoTelefone.classList.remove("invalido");
-  campoSmall.classList.remove("negado");
-  campoTelefone.classList.remove("valido");
-
-  campoMensagem.classList.remove("invalido");
-  campoSmall.classList.remove("negado");
-  campoMensagem.classList.remove("valido");
-}
