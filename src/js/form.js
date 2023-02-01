68650b11c969776bbdb7d17874b85d9cde24734f
n3ytan <109997116+N3ytan@users.noreply.github.com>
2023-01-31 21:17:16 -0300

    adjusts

--- src/js/form.js
+++ src/js/form.js
@@ -7,14 +7,14 @@ console.log(campoSmall)
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   inputs.forEach((input, index) => {
-    
     if (input.value === "") {
+      input.classList.remove("valido")
       input.classList.add("invalido");
       campoSmall[index].classList.add("negado");
-      console.log(index);
     } else {
       input.classList.add("valido");
       campoSmall[index].classList.remove("negado");
+
     }
   });
 });
